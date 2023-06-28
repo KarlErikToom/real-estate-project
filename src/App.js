3d0dd0952bd8640f2acb45c0dde2a881d8404109
Karl <toom.karl42@gmail.com>
2023-06-28 19:09:35 +0300

    added about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Landing from "./components/Landing";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Landing />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import AboutUs from "../assets/image2.avif";
+
+function About() {
+  return (
+    <section id="about">
+      <div className="container">
+        <div className="row">
+          <div className="about__wrapper">
+            <div className="about__left">
+              <img className="about__left--img" src={AboutUs} alt="" />
+            </div>
+            <div className="about__right">
+              <div className="about__right--text">
+                <span className="about__span">About Us</span>
+                <h1 className="about__header">Who is Homefy</h1>
+                <p className="about__para">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Cupiditate provident totam eius. Obcaecati ullam, ipsum ad id
+                  sed, est quibusdam quo, magnam corrupti exercitationem
+                  aliquid! Iusto corrupti sint debitis earum?
+                </p>
+              </div>
+              <div className="about__info--wrapper">
+                <span className="info1">price</span>
+                <span className="info2">quality</span>
+                <span className="info3">speed</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
