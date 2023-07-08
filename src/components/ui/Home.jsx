0af0bcb6e71527df989fad859675b75d94a1b0ae
Karl <toom.karl42@gmail.com>
2023-07-08 21:06:08 +0300

    got the property checkboxes to work

--- src/components/ui/Home.jsx
+++ src/components/ui/Home.jsx
@@ -8,13 +8,17 @@ function Home({ property }) {
           <img className="home__img" src={property.image_url} alt="" />
         </a>
       </figure>
-
       <div className="home__name">
         <a href="/" className="home__name--link">
           {property.listing_name}
         </a>
       </div>
-      <span className="home__price">{property.listing_price}</span>
+      <div className="home__description">
+        <span className="home__price">{property.listing_price.toLocaleString()}$</span>
+        <span className="home__bedrooms">{property.bedrooms} Bedrooms</span>
+        <span className="home__bathrooms">{property.bathrooms} Bathrooms</span>
+        <span className="home__garage">{property.garage_size} Garage</span>
+      </div>
     </div>
   );
 }
--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -1,41 +1,113 @@
 import React, { useState } from "react";
 import PriceRangeSlider from "../components/ui/PriceRangeSlider";
 import Bedroom from "../components/ui/Bedroom";
+import Home from "../components/ui/Home";
 
 function Properties({ properties }) {
+  const [isChecked, setIsChecked] = useState({
+    APARTMENT: false,
+    CONDOMINIUM: false,
+    TOWNHOUSE: false,
+    SINGLE_FAMILY: false,
+  });
+  const [filteredProperties, setFilteredProperties] = useState(properties);
+
+  function handleOnChange(event) {
+    const { value, checked } = event.target;
+    setIsChecked((prevState) => ({
+      ...prevState,
+      [value]: checked,
+    }));
+  }
+
+  function filterBox(event) {
+    event.preventDefault();
+
+    const filteredData = properties.filter((property) => {
+      return (
+        (isChecked.APARTMENT && property.property_type === "Apartment") ||
+        (isChecked.CONDOMINIUM && property.property_type === "Condominium") ||
+        (isChecked.TOWNHOUSE && property.property_type === "Townhouse") ||
+        (isChecked.SINGLE_FAMILY &&
+          property.property_type === "Single Family Home")
+      );
+    });
+
+    setFilteredProperties(filteredData);
+  }
+
   return (
     <section id="properties">
       <div className="container">
         <div className="row">
-          <aside>
-            <form className="properties__form">
-              <h1>Property type:</h1>
-              <label>
-                <input className="properties__input" type="checkbox" value="APARTMENT" />
-                Apartments
-              </label>
-              <br />
-              <label>
-                <input className="properties__input" type="checkbox" value="CONDOMINIUM" />
-                Condominiums
-              </label>
-              <br />
-              <label>
-                <input className="properties__input" type="checkbox" value="TOWNHOUSE" />
-                Townhouses
-              </label>
-              <br />
-              <label>
-                <input className="properties__input" type="checkbox" value="SINGLE_FAMILY" />
-                Single Family Homes
-              </label>
-              <PriceRangeSlider />
-              <Bedroom />
-              <button type="submit" className="btn submit__btn">
-                Search
-              </button>
-            </form>
-          </aside>
+          <div className="properties__wrapper">
+            <aside>
+              <form className="properties__form">
+                <h1>Property type:</h1>
+                <label>
+                  <input
+                    id="filter"
+                    className="properties__input"
+                    type="checkbox"
+                    value="APARTMENT"
+                    checked={isChecked.APARTMENT}
+                    onChange={handleOnChange}
+                  />
+                  Apartments
+                </label>
+                <br />
+                <label>
+                  <input
+                    id="filter"
+                    className="properties__input"
+                    type="checkbox"
+                    value="CONDOMINIUM"
+                    checked={isChecked.CONDOMINIUM}
+                    onChange={handleOnChange}
+                  />
+                  Condominiums
+                </label>
+                <br />
+                <label>
+                  <input
+                    id="filter"
+                    className="properties__input"
+                    type="checkbox"
+                    value="TOWNHOUSE"
+                    checked={isChecked.TOWNHOUSE}
+                    onChange={handleOnChange}
+                  />
+                  Townhouses
+                </label>
+                <br />
+                <label>
+                  <input
+                    id="filter"
+                    className="properties__input"
+                    type="checkbox"
+                    value="SINGLE_FAMILY"
+                    checked={isChecked.SINGLE_FAMILY}
+                    onChange={handleOnChange}
+                  />
+                  Single Family Homes
+                </label>
+                <PriceRangeSlider />
+                <Bedroom />
+                <button
+                  type="submit"
+                  onClick={filterBox}
+                  className="btn submit__btn"
+                >
+                  Search
+                </button>
+              </form>
+            </aside>
+            <div className="properties">
+              {filteredProperties.map((property) => (
+                <Home property={property} key={property.id} />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </section>
